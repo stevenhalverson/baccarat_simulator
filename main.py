cbca7f48edfc584af12c4be83a1d5fd02995f063
Steven Halverson <sghalverson@gmail.com>
2024-01-06 04:01:22 -0800

    Added progress bar.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import tkinter as tk
 from tkinter import messagebox
+from tkinter import ttk
 from simulation import BaccaratSimulation
 import pandas as pd
 from data_access import *
 import threading
+import queue
 
 class BaccaratGUI:
     def __init__(self, root):
@@ -25,16 +27,32 @@ class BaccaratGUI:
         exit_button = tk.Button(root, text="Exit", command=root.destroy)
         exit_button.pack(pady=10)
 
+        #Progress bar
+        self.progress_queue = queue.Queue()
+        self.progress = ttk.Progressbar(root, orient="horizontal", length=300, mode="determinate")
+        self.progress.pack(pady=20)
+
     def start_simulation(self):
         try:
             num_shoes = int(self.shoes_entry.get())
-            sim = BaccaratSimulation()
+            sim = BaccaratSimulation(progress_queue=self.progress_queue)
 
             simulation_thread = threading.Thread(target=self.run_simulation, args=(sim, num_shoes))
             simulation_thread.start()
+
+            self.check_progress() #should this go here?
             
         except ValueError:
             messagebox.showerror("Error", "Invalid number of shoes. Please enter a valid number.")
+
+    def check_progress(self):
+        try:
+            progress = self.progress_queue.get_nowait()
+            self.progress["value"] = progress
+
+        except queue.Empty:
+            pass
+        self.root.after(100, self.check_progress)
     
     def run_simulation(self, sim, num_shoes):
             sim.begin_simulation(shoes=num_shoes)
