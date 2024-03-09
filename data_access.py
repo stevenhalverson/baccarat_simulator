49f025f12344de2679df55c3d9250da5dcf1f710
Steven Halverson <sghalverson@gmail.com>
2024-03-09 13:46:34 -0800

    improved database schema
    Added a proper index to the database. There are still issues. The main issue known currently is that there are no values in the database when a third card is not drawn in a round of baccarat.

--- data_access.py
+++ data_access.py
@@ -33,9 +33,21 @@ def save_to_db(df, db, append: bool = True):
     
     create_date = datetime.now()
     df['create_date'] = create_date
-        # using shape to get column and row counts
+    if not append:
+            with db.connect() as conn:
+                conn.execute(f"DROP TABLE IF EXISTS simulation_results")
+
+        # Add an index column to serve as the primary key
+    df.reset_index(inplace=True)
+    df.rename(columns={'index': 'id'}, inplace=True)
+
     rows, columns = df.shape
     appending = 'append' if append else 'replace'
     print(f'loading df to db, {rows} row(s)')
     df.to_sql('simulation_results', db, index=False, if_exists=appending)
     print('data loaded successfully!')
+
+        # Add a primary key constraint to the 'id' column if creating a new table
+    if not append:
+        with db.connect() as conn:
+            conn.execute(f"ALTER TABLE simulation_results ADD PRIMARY KEY (id)")
